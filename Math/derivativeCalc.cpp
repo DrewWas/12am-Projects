ec6a6c0c1fb832bc0c17bc83f85e530c93fd9705
Andrew Wasielewski <awwasielewski@gmail.com>
2022-01-13 18:55:40 -0500

    wow learning c++.. kinda

--- Math/derivativeCalc.cpp
+++ Math/derivativeCalc.cpp
@@ -1,39 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-	std::cout << "Hello World!\n\n";
+
+int main(){
+	string func;
+	system("clear");
+	cout << "Input the function you would like to differentiate\n"; 
+	cout << "Use () to seperate multiplication and ^ for exponents\n\n";
+	cout << "-  ";
+	cin >> func;
+	cout << "\n\nThe differentiated function is - " + func + "\n\n";
 	return 0;
-
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Parse the function
+//
+// See which rule is needed where 
+//
+// call rules accordingly
+//
+// This is super high level... Ill probably realize how hard this is once I actually get started
 
 
