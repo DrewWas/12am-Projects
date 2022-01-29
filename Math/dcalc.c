d08cbc8a66b7c3fb2552372a4a694d01f5a616ed
Andrew Wasielewski <awwasielewski@gmail.com>
2022-01-29 16:02:21 -0500

    fuck sp, this time in c

--- Math/dcalc.c
+++ Math/dcalc.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+    char command[50];
+    strcpy( command, "clear");
+    system(command);
     printf("Fuck SP\n");
     return 0;
 
 
 }
+
+
+
+
+/* Parse the function and return each 'part'' */
+
+/* Recognize with derivation rule must be applied and apply it (this means writing a funciton for   quotint rule, power rule, etc.) */
+
+/* Return parent and differentiated function */
